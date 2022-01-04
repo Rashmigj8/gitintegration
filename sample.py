90bfb716c7f01734f2173430c64c9809211eee17
Rashmigj8 <97016873+Rashmigj8@users.noreply.github.com>
2022-01-04 14:30:35 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,3 +2,7 @@ print("welcome to LG!!!----fh")
 print("hi:")
 print("done-kdklstgffg")
 print("rashmip")
+string1 = "Linux"
+string2 = "Hint"
+joined_string = string1 + string2
+print(joined_string)
